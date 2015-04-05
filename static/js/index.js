7444f65ef978e973b276945430876eb3b6979eb7
Sky <dtlsky@gmail.com>
2015-04-05 01:59:18 -0700

    Add full functionality for "play" button

--- static/js/index.js
+++ static/js/index.js
@@ -3,8 +3,20 @@
 */
 
 $( document ).ready(function(){
+
+  var date = new Date();
+  var month = date.getMonth();
+  var day = date.getDate();
+
+  $("#slider").dateRangeSlider({
+     bounds:{
+      min: new Date(2015, 0, 1),
+      max: new Date(date.getFullYear(), month, day)
+    }
+  });
+
   initialize();
-  //showLatest();
+    showLatest();
     $(function(){
      /* $().timelinr({
       autoPlay: 'true',
@@ -17,6 +29,8 @@ $( document ).ready(function(){
   });
 
   $("#stop").click(function(){
+    $("#dates").empty();
+    $("#subtitle").empty();
     alert("stop");
      $(function(){
         $().timelinr({
@@ -24,27 +38,104 @@ $( document ).ready(function(){
       })
     });
 
+    showLatest();
+
   });
 
-  $("#slider").dateRangeSlider();
+
 
   $('#play').click(function(){
-    alert("date selected");
+    $("#subtitle").empty();
+    //alert("date selected");
     var speed = $("#SliderSingle").slider("value");
-    alert("The speed is "+ 1000*speed);
+    //alert("The speed is "+ 1000*speed);
     //$("#slider").dateRangeSlider("values", new Date(2015, 0, 1), new Date(2015, 3, 31));
     var dateMax = $("#slider").dateRangeSlider("max");
     var dateMin = $("#slider").dateRangeSlider("min");
-    alert("min: "+dateMin.toString() + " " + "max: "+dateMax.toString());
+    //alert("min: "+dateMin + " " + "max: "+dateMax);
 
-    $(function(){
-        $().timelinr({
-        autoPlay: 'true',
-        autoPlayDirection: 'forward',
-        startAt: 1,
-        autoPlayPause:1000*speed
-      })
+    var dateMins = dateMin.toString().split(" ");
+    var dateMinDay = dateMins[2];
+    var dateMinMonth = dateMins[1];
+    var dateMinMonth = "JanFebMarAprMayJunJulAugSepOctNovDec".indexOf(dateMinMonth) / 3 + 1;
+    var dateMinYear = dateMins[3];
+
+    var time_query_Min = dateMinYear + '_' +
+    (dateMinMonth<10 ? '0' : '') + dateMinMonth + '_' + dateMinDay;
+
+    //alert(time_query_Min);
+
+  
+    var dateMaxs = dateMax.toString().split(" ");
+    var dateMaxDay = dateMaxs[2];
+    var dateMaxMonth = dateMaxs[1];
+    var dateMaxMonth = "JanFebMarAprMayJunJulAugSepOctNovDec".indexOf(dateMaxMonth) / 3 + 1;
+    var dateMaxYear = dateMaxs[3];
+
+
+    var time_query_Max = dateMaxYear + '_' +
+    (dateMaxMonth<10 ? '0' : '') + dateMaxMonth + '_' + dateMaxDay;
+    var imageAPI = "http://127.0.0.1:8080/no2?start_date="+time_query_Min+"&end_date="+time_query_Max;
+
+    alert(imageAPI);
+    $("#dates").empty();
+
+
+    $.ajax({
+      type: "GET",
+      url: imageAPI,
+      //contentType: "application/json; charset:utf-8",
+      dataType: "json",
+      success: function (data) {
+        for(var i = 0; i < data.result.length; i++) {
+          var datainfo = data.result[i].toString().split(",");
+          var dateFormats = datainfo[0].split("_");
+          var dateFormat = dateFormats[0]+"/"+dateFormats[1]+"/"+dateFormats[2];
+          //alert(datainfo[0]+" url: "+datainfo[1]);
+          var listItem = "<li><a href=\""+"#"+datainfo[0]+"\" onclick=\"setmap('"+datainfo[1]+"')\">"+dateFormat+"</a></li>";
+          //alert(listItem);
+          $("#timeline ul").append(listItem);
+        }
+
+        $("#dates li").css({"list-style":"none","float":"left","width":"100px","height":"50px","font-size":"14px","text-align":"center","background":"url('../images/biggerdot.png') center bottom no-repeat"});
+        $("#dates a").css({"line-height":"45px;","padding-bottom":"10px","color":"#ffffcc","text-decoration":"none","-webkit-transition":"0.5s","-moz-transition":"0.5s","-o-transition":"0.5s","-ms-transition":"0.5s","transition":"0.5s"});
+        $("#dates .selected").css("font-size","18px");
+
+        $(function(){
+            $().timelinr({
+            autoPlay: 'true',
+            autoPlayDirection: 'forward',
+            startAt: 1,
+            autoPlayPause:1000*speed
+          })
+        });
+
+      },
+      error: function (fs) {
+        alert("error");
+      },
     });
+
+
+/*  alert("Min Modified: "+ time_query_Min);
+    alert("Max Modified: "+ time_query_Max);
+*/
+    // Generate Data 
+   
+    while(dateMin <= dateMax){
+
+
+        //var imageAPI = "http://127.0.0.1:8080/no2?adate=2015_01_01";
+
+        dateMin = new Date(dateMin.setDate(
+                dateMin.getDate() + 1
+        ))
+
+        
+    }
+
+
+
   })
 
 });
@@ -63,15 +154,15 @@ function showLatest(){
     id: 'heading',
     style: 'text-align:center',
     text: time,
-  }).appendTo('#dates');
+  }).appendTo('#subtitle');
 
   // Ask for the latest data 
   var time_query = date.getFullYear() + '_' +
     (month<10 ? '0' : '') + month + '_' +
     (day<10 ? '0' : '') + day;
 
-  var imageAPI = "http://127.0.0.1:8080/no2?date="+time_query;
-  var imageAPI = "http://127.0.0.1:8080/no2?date=2015_01_01";
+  var imageAPI = "http://127.0.0.1:8080/no2?adate="+time_query;
+  //var imageAPI = "http://127.0.0.1:8080/no2?adate=2015_01_01";
 
 
   $.ajax({
@@ -88,13 +179,6 @@ function showLatest(){
       alert("error");
     }
   });
- 
-
-  //setmap('2015.png');
-
-   $("#search").click(function(){
-        alert("hi"); 
-    });
   
 }
 
