0cdf0221aa09453319510b61c9b6b5e83b075fe3
Sky <dtlsky@gmail.com>
2015-04-13 14:31:32 -0700

    Add validation to search box

--- static/js/index.js
+++ static/js/index.js
@@ -51,7 +51,7 @@ $( document ).ready(function(){
 
 
   $('#play').click(function(){
-    $("#subtitle").empty();
+   
     //alert("date selected");
     var speed = $("#SliderSingle").slider("value");
     //alert("The speed is "+ 1000*speed);
@@ -82,10 +82,7 @@ $( document ).ready(function(){
     var time_query_Max = dateMaxYear + '_' +
     (dateMaxMonth<10 ? '0' : '') + dateMaxMonth + '_' + dateMaxDay;
     var imageAPI = "http://127.0.0.1:8080/"+search_data+"?start_date="+time_query_Min+"&end_date="+time_query_Max;
-
-    //alert(imageAPI);
     $("#dates").empty();
-
 
     $.ajax({
       type: "GET",
@@ -93,6 +90,11 @@ $( document ).ready(function(){
       //contentType: "application/json; charset:utf-8",
       dataType: "json",
       success: function (data) {
+        if(data.result == "invalid"){
+          alert("The data doesn't exist in our database");
+        }
+        else {
+        $("#subtitle").empty();
         for(var i = 0; i < data.result.length; i++) {
           var datainfo = data.result[i].toString().split(",");
           var dateFormats = datainfo[0].split("_");
@@ -115,6 +117,7 @@ $( document ).ready(function(){
             autoPlayPause:1000*speed
           })
         });
+        }
 
       },
       error: function (fs) {
@@ -210,24 +213,13 @@ var showed = false;
 
 function initialize() {
   var vancouver = new google.maps.LatLng(49.1,-123.0);
-  //var chicago = new google.maps.LatLng(41.875696,-87.624207);
+
   var mapOptions = {
     zoom: 9,
     center: vancouver
   }
 
   map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-
-  //setmap('test.png');
-
-  /*
-  var ctaLayer = new google.maps.KmlLayer({
-    //url: 'http://gmaps-samples.googlecode.com/svn/trunk/ggeoxml/cta.kml'
-    //url: 'http://localhost:8000/NO_len_en.kmz'
-    url: 'http://ws.mss.icics.ubc.ca/~jcchen25/NO_len_en.kmz'
-  });
-  ctaLayer.setMap(map);
-  */
 }
 
 function setmap(str) {
@@ -244,12 +236,13 @@ function setmap(str) {
 
   showed = true;
 
-
   overlay = new google.maps.GroundOverlay(
       str,
       imageBounds, overlayOpts);
-  overlay.setMap(null);
   overlay.setMap(map);
+
+
+
 }
 
 function myfunc() {
